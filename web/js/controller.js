291757ef233a2a72c7fdbb97412bcd05fbf48cce
Robb <robbchar@gmail.com>
2014-04-10 12:14:31 -0700

    getting the item on the scoe

--- web/js/controller.js
+++ web/js/controller.js
@@ -7,8 +7,17 @@ appController.controller('ListController',  function($scope, $http) {
 });
 
 appController.controller('DetailsController',  function($scope, $http, $routeParams) {
+  function getAppById(id, apps) {
+    for(var i = 0; i < apps.length; i++) {
+      if(apps[i].name.toLowerCase() === id.toLowerCase()) {
+        return apps[i];
+      }
+    }
+  }
+
   $http.get('js/data.json').success(function(data) {
     $scope.apps = data;
     $scope.whichItem = $routeParams.itemId;
+    $scope.item = getAppById($routeParams.itemId, data);
   });
 });
